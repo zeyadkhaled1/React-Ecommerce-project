e21a636dfcb21c388a334e438f20729c6ea60040
ahmed1532 <am1009374@gmail.com>
2023-06-17 01:15:07 +0300

    design adjustments

--- src/Components/Admin/AdminSideBar.js
+++ src/Components/Admin/AdminSideBar.js
@@ -6,47 +6,47 @@ export const AdminSideBar = () => {
 		<div className='sidebar'>
 			<div className='d-flex flex-column'>
 				<Link to='/admin/allorders' style={{ textDecoration: 'none' }}>
-					<div className='admin-side-text mt-3 border-bottom p-2 mx-auto text-center'>
+					<div className='admin-side-text mt-3 border-bottom p-2 mx-auto text-center'id={window.location.pathname === '/admin/allorders'?"active":""}>
 						اداره الطلبات
 					</div>
 				</Link>
 
 				<Link to='/admin/allproducts' style={{ textDecoration: 'none' }}>
-					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'>
+					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'id={window.location.pathname === '/admin/allproducts'?"active":""}>
 						اداره المنتجات
 					</div>
 				</Link>
 
 				<Link to='/admin/all-coupon' style={{ textDecoration: 'none' }}>
-					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'>
+					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'id={window.location.pathname === '/admin/all-coupon' ?"active":""}>
 						اداره الكوبونات
 					</div>
 				</Link>
 
 				<Link to='/admin/all-vendor-request' style={{ textDecoration: 'none' }}>
-					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'>
+					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'id={window.location.pathname === '/admin/all-vendor-request'?"active":""}>
 						طلبات المورد
 					</div>
 				</Link>
 
 				<Link to='/admin/addbrand' style={{ textDecoration: 'none' }}>
-					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'>
+					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'id={window.location.pathname === '/admin/addbrand'?"active":""}>
 						اضف ماركه
 					</div>
 				</Link>
 
 				<Link to='/admin/addcategory' style={{ textDecoration: 'none' }}>
-					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'>
+					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'id={window.location.pathname === '/admin/addcategory' ?"active":""}>
 						اضف تصنيف
 					</div>
 				</Link>
 
 				<Link to='/admin/addproduct' style={{ textDecoration: 'none' }}>
-					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'>اضف منتج</div>
+					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'id={window.location.pathname === '/admin/addproduct' ?"active":""}>اضف منتج</div>
 				</Link>
 
 				<Link to='/admin/add-coupon' style={{ textDecoration: 'none' }}>
-					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'>
+					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'id={window.location.pathname === '/admin/add-coupon'?"active":""}>
 						اضف كوبون
 					</div>
 				</Link>
--- src/Components/Vendor/VendorSideBar.js
+++ src/Components/Vendor/VendorSideBar.js
@@ -6,29 +6,29 @@ export const VendorSideBar = () => {
 		<div className='sidebar'>
 			<div className='d-flex flex-column'>
 				<Link to='/vendor/all-orders' style={{ textDecoration: 'none' }}>
-					<div className='admin-side-text mt-3 border-bottom p-2 mx-auto text-center'>
+					<div className='admin-side-text mt-3 border-bottom p-2 mx-auto text-center' id={window.location.pathname === '/vendor/all-orders'?"active":""}>
 						اداره الطلبات
 					</div>
 				</Link>
 
 				<Link to='/vendor/all-products' style={{ textDecoration: 'none' }}>
-					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'>
+					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'id={window.location.pathname === '/vendor/all-products'?"active":""}>
 						اداره المنتجات
 					</div>
 				</Link>
 
 				<Link to='/vendor/all-coupon' style={{ textDecoration: 'none' }}>
-					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'>
+					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'id={window.location.pathname === '/vendor/all-coupon'?"active":""}>
 						اداره الكوبونات
 					</div>
 				</Link>
 
 				<Link to='/vendor/add-product' style={{ textDecoration: 'none' }}>
-					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'>اضف منتج</div>
+					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'id={window.location.pathname === '/vendor/add-product'?"active":""}>اضف منتج</div>
 				</Link>
 
 				<Link to='/vendor/add-coupon' style={{ textDecoration: 'none' }}>
-					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'>
+					<div className='admin-side-text my-1 border-bottom p-2 mx-auto text-center'id={window.location.pathname === '/vendor/add-coupon'?"active":""}>
 						اضف كوبون
 					</div>
 				</Link>
